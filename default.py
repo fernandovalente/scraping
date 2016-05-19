25fd0a1d09be6a5d1fafd061fc3ddd2b1168f441
Fernando Valente <fernandovalentedev@gmail.com>
2016-05-19 19:23:29 -0300

    criando requirements.txt

--- default.py
+++ default.py
@@ -4,6 +4,5 @@ from urllib.request import urlopen
 
 def open_and_returns_bs4_object(url):
     """ Receives a url and return an BS4 object """
-
     html = urlopen(url)
     return BeautifulSoup(html)
