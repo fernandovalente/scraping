902f4e58b86f04ed328f9165491380b3b9f89d2a
Fernando Valente <fernandovalentedev@gmail.com>
2016-05-19 22:16:25 -0300

    honda ok

--- default.py
+++ default.py
@@ -1,8 +1,22 @@
+import json
+import codecs
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 
 def open_and_returns_bs4_object(url):
-    """ Receives a url and return an BS4 object """
     html = urlopen(url)
     return BeautifulSoup(html)
+
+
+def create_json_file(data, factory_name):
+
+    json_file = '{}.json'.format(factory_name)
+
+    try:
+        with codecs.open(json_file, 'w', encoding='utf-8') as outfile:
+            json.dump(data, outfile, ensure_ascii=False)
+    except Exception as e:
+        raise e
+
+    return 'Created {}'.format(json_file)
--- honda.py
+++ honda.py
@@ -1,14 +1,70 @@
-from default import open_and_returns_bs4_object
+from default import open_and_returns_bs4_object, create_json_file
 
 
 urls = [
     'http://www.honda.com.br/motos/Paginas/pop110i.aspx',
+    'http://www.honda.com.br/motos/Paginas/Biz110i.aspx',
     'http://www.honda.com.br/motos/Paginas/nova-lead-110.aspx',
-    'http://www.honda.com.br/motos/Paginas/Biz-125.aspx'
+    'http://www.honda.com.br/motos/Paginas/PCX.aspx',
+    'http://www.honda.com.br/motos/Paginas/cg-125i-fan.aspx',
+    'http://www.honda.com.br/motos/Paginas/cg-125-cargo.aspx',
+    'http://www.honda.com.br/motos/Paginas/CRF150F.aspx',
+    'http://www.honda.com.br/motos/Paginas/CG160Cargo.aspx',
+    'http://www.honda.com.br/motos/Paginas/CG160Start.aspx',
+    'http://www.honda.com.br/motos/Paginas/cg160fan.aspx',
+    'http://www.honda.com.br/motos/Paginas/cg160titan.aspx',
+    'http://www.honda.com.br/motos/Paginas/nxr160bros.aspx',
+    'http://www.honda.com.br/motos/Paginas/XRE190.aspx',
+    'http://www.honda.com.br/motos/Paginas/crf-230f.aspx',
+    'http://www.honda.com.br/motos/Paginas/cbtwister.aspx',
+    'http://www.honda.com.br/motos/Paginas/sh300i.aspx',
+    'http://www.honda.com.br/motos/Paginas/NC750X.aspx',
+    'http://www.honda.com.br/motos/Paginas/CB1000R.aspx',
+    'http://www.honda.com.br/motos/Paginas/trx-420-fourtrax.aspx',
+    'http://www.honda.com.br/motos/Paginas/cbr-600rr.aspx',
+    'http://www.honda.com.br/motos/Paginas/cbr-1000rr-fireblade-2011.aspx',
+    'http://www.honda.com.br/motos/Paginas/gl-1800-gold-wing.aspx',
 ]
+
+motos = dict()
 
 for url in urls:
     obj = open_and_returns_bs4_object(url)
-    motorcycle = obj.find('div', {'class': 'NomeVeiculo'}).get_text().strip()
 
-    print(motorcycle)
+    nome = obj.find('div', {'class': 'NomeVeiculo'}).get_text().strip()
+    foto = obj.find('div', {'id': 'ctl00_PlaceHolderMain_PageImage__ControlWrapper_RichImageField'})
+
+    data = dict(
+        nome=nome,
+        foto='{}{}'.format(
+            'http://www.honda.com.br',
+            foto.find('img').get('src')
+        ),
+        ficha_tecnica=[]
+    )
+
+    # Propriedades da moto
+    table = obj.find('table', {'class', 'ms-rteTable-EspecificacoesTecnicas'})
+
+    for tr in table.findAll({'tr'}):
+        td = tr.findAll({'td'})
+
+        data['ficha_tecnica'].append(
+            [
+                td[0].getText().strip().replace('\n', ''),
+                td[1].getText().strip().replace('\n', '')
+            ]
+        )
+
+        td3 = td[3].getText().strip().replace('\n', '')
+        try:
+            td4 = td[4].getText().strip().replace('\n', '')
+        except:
+            td4 = ''
+
+        data['ficha_tecnica'].append([td3, td4])
+
+    print(nome)
+    motos[nome] = data
+
+create_json_file(motos, 'honda')
